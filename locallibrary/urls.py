b624d393ceee073ae171816f29b8b63ee6f5d8df
bertontin <bertontin@gmail.com>
2023-11-02 05:06:26 +0000

    chang application and project name

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,4 +1,4 @@
-"""hello_world URL Configuration
+"""locallibrary URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
